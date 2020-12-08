9ec2e8324a9c0244d8eb9e96a13c5b231eeb6710
Iann <iisackssonoliveira@gmail.com>
2020-12-07 23:45:07 -0300

    User authentication

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
 
@@ -12,6 +13,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errors());
